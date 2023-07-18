89c24fecc56c2e2557416216a2d25a22b57dfa3e
Hernando Abella <hernandoabella@gmail.com>
2023-07-18 13:36:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const reloj = document.querySelector('#reloj');
 
 const mecanismo = () => {
-   
-   let tiempo = new Date();
-   let horas = (tiempo.getHours() % 12);
-   let minutos = tiempo.getMinutes();
-   let segundos = tiempo.getSeconds();
+  let tiempo = new Date();
+  let horas = tiempo.getHours() % 12;
+  let minutos = tiempo.getMinutes();
+  let segundos = tiempo.getSeconds();
+  let periodo = tiempo.getHours() >= 12 ? 'PM' : 'AM';
 
-   if (horas.length < 2) {
-      horas = '0' + horas;
-   }
+  if (horas < 10) {
+    horas = '0' + horas;
+  }
 
-   if (minutos.length < 2) {
-      minutos = '0' + minutos;
-   }
+  if (minutos < 10) {
+    minutos = '0' + minutos;
+  }
 
-   if (segundos.length < 2) {
-      segundos = '0' + segundos;
-   }
+  if (segundos < 10) {
+    segundos = '0' + segundos;
+  }
 
-   let cadenaTxt = horas + ' : ' + minutos + ' : ' + segundos;
+  let cadenaTxt = horas + ' : ' + minutos + ' : ' + segundos + ' ' + periodo;
 
-   reloj.textContent = cadenaTxt;
-}
+  reloj.textContent = cadenaTxt;
+};
 
 setInterval(mecanismo, 1000);
